0fc1e516e4344d8c86c43ed5587e90ff50135755
nehayaligar <nehayaligar03@gmail.com>
2025-04-22 14:17:03 +0530

    Save in file

---
+++ Employee.java
@@ -0,0 +1,16 @@
+public class Employee {
+    public static void main(String[] args){
+        String name = "Madhumati";
+        int id  = 11;
+        int salary = 20000;
+        String company= "KLE";
+
+        System.out.println("----EMPLOYEE DETAILS-----");
+        System.out.println("Employee name:" +name);
+        System.out.println("Employee id:" +id);
+        System.out.println("Employee salary:" +salary);
+        System.out.println("Employee company:" +company);
+        
+    }
+
+}
