df5cdfe1b161e2561eb0366d2ce875025b8a4b22
nehayaligar <nehayaligar03@gmail.com>
2025-04-22 14:38:43 +0530

    Save in main

--- Employee.java
+++ Employee.java
@@ -4,8 +4,6 @@ public class Employee {
         int id  = 11;
         int salary = 20000;
         String company= "KLE";
-
-        System.out.println("----EMPLOYEE DETAILS-----");
         System.out.println("Employee name:" +name);
         System.out.println("Employee id:" +id);
         System.out.println("Employee salary:" +salary);
