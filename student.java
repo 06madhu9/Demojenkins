ccef8a6b4beb423dee098a3cd2448e2e6f5908a1
nehayaligar <nehayaligar03@gmail.com>
2025-04-22 15:53:07 +0530

    save it in main

---
+++ student.java
@@ -0,0 +1,14 @@
+public class student {
+
+    public static void main(String[] args){
+        String name = "Revathi";
+        int usn  = 113;
+        int fees = 80000;
+        String college= "KLETECH";
+        System.out.println("Stuent name:" +name);
+        System.out.println("Student usn:" +usn);
+        System.out.println("Student fees:" +fees);
+        System.out.println("Student college:" +college);
+        
+}
+}
